00929b184d0b2981ba21b3cbdebb9beeb26b807c
Yacine <Yacine@192.168.1.28>
2014-05-29 14:14:29 +0200

    Petite Correction CarteImages

--- ProjetAndroidLP/src/com/example/projetandroidlp/CarteImages.java
+++ ProjetAndroidLP/src/com/example/projetandroidlp/CarteImages.java
@@ -1,22 +1,4 @@
-/*
-    BlackJack Trainer.  BJ strategy tutor.
-    Copyright (C) 2012  Daniel Kraft <d@domob.eu>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-package eu.domob.bjtrainer;
+package com.example.projetandroidlp;
 
 import android.content.res.Resources;
 
@@ -89,7 +71,7 @@ public class CarteImages
    * @param c The card we want to draw.
    * @return Drawable for it.
    */
-  public Drawable getCarte(Card c)
+  public Drawable getCarte(Carte c)
   {
     int couleur = 0;
     switch (c.couleur)
@@ -119,7 +101,7 @@ public class CarteImages
         case Carte.ROI:
           type = 1;
           break;
-        case Carte.REINE:
+        case Carte.DAME:
           type = 2;
           break;
         case Carte.VALET:
@@ -138,60 +120,60 @@ public class CarteImages
   private static final int[] IDs = new int[]
     {
       -1
-, R.drawable.card_1
-, R.drawable.card_2
-, R.drawable.card_3
-, R.drawable.card_4
-, R.drawable.card_5
-, R.drawable.card_6
-, R.drawable.card_7
-, R.drawable.card_8
-, R.drawable.card_9
-, R.drawable.card_10
-, R.drawable.card_11
-, R.drawable.card_12
-, R.drawable.card_13
-, R.drawable.card_14
-, R.drawable.card_15
-, R.drawable.card_16
-, R.drawable.card_17
-, R.drawable.card_18
-, R.drawable.card_19
-, R.drawable.card_20
-, R.drawable.card_21
-, R.drawable.card_22
-, R.drawable.card_23
-, R.drawable.card_24
-, R.drawable.card_25
-, R.drawable.card_26
-, R.drawable.card_27
-, R.drawable.card_28
-, R.drawable.card_29
-, R.drawable.card_30
-, R.drawable.card_31
-, R.drawable.card_32
-, R.drawable.card_33
-, R.drawable.card_34
-, R.drawable.card_35
-, R.drawable.card_36
-, R.drawable.card_37
-, R.drawable.card_38
-, R.drawable.card_39
-, R.drawable.card_40
-, R.drawable.card_41
-, R.drawable.card_42
-, R.drawable.card_43
-, R.drawable.card_44
-, R.drawable.card_45
-, R.drawable.card_46
-, R.drawable.card_47
-, R.drawable.card_48
-, R.drawable.card_49
-, R.drawable.card_50
-, R.drawable.card_51
-, R.drawable.card_52
-, R.drawable.card_53
-, R.drawable.card_54
+		, R.drawable.card_1
+		, R.drawable.card_2
+		, R.drawable.card_3
+		, R.drawable.card_4
+		, R.drawable.card_5
+		, R.drawable.card_6
+		, R.drawable.card_7
+		, R.drawable.card_8
+		, R.drawable.card_9
+		, R.drawable.card_10
+		, R.drawable.card_11
+		, R.drawable.card_12
+		, R.drawable.card_13
+		, R.drawable.card_14
+		, R.drawable.card_15
+		, R.drawable.card_16
+		, R.drawable.card_17
+		, R.drawable.card_18
+		, R.drawable.card_19
+		, R.drawable.card_20
+		, R.drawable.card_21
+		, R.drawable.card_22
+		, R.drawable.card_23
+		, R.drawable.card_24
+		, R.drawable.card_25
+		, R.drawable.card_26
+		, R.drawable.card_27
+		, R.drawable.card_28
+		, R.drawable.card_29
+		, R.drawable.card_30
+		, R.drawable.card_31
+		, R.drawable.card_32
+		, R.drawable.card_33
+		, R.drawable.card_34
+		, R.drawable.card_35
+		, R.drawable.card_36
+		, R.drawable.card_37
+		, R.drawable.card_38
+		, R.drawable.card_39
+		, R.drawable.card_40
+		, R.drawable.card_41
+		, R.drawable.card_42
+		, R.drawable.card_43
+		, R.drawable.card_44
+		, R.drawable.card_45
+		, R.drawable.card_46
+		, R.drawable.card_47
+		, R.drawable.card_48
+		, R.drawable.card_49
+		, R.drawable.card_50
+		, R.drawable.card_51
+		, R.drawable.card_52
+		, R.drawable.card_53
+		, R.drawable.card_54
     };
 
 }
