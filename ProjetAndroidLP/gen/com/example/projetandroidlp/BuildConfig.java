d88bc6e5a2a7c633492b19b8b90cbdbf6ba13f9c
yacine <yacine.aitmaamar@gmail.com>
2014-06-10 14:25:40 +0200

    problème réglé commit

--- ProjetAndroidLP/gen/com/example/projetandroidlp/BuildConfig.java
+++ ProjetAndroidLP/gen/com/example/projetandroidlp/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package eu.domob.bjtrainer;
+package com.example.projetandroidlp;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
