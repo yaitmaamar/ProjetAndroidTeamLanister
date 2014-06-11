ca2eae1fe50f2e840682dffea3ec67d74536c7b4
will <will@will-MSI>
2014-06-11 20:06:46 +0200

    reset du gain ok nettoyage du fichier

--- ProjetAndroidLP/src/com/example/projetandroidlp/StrategieCoach.java
+++ ProjetAndroidLP/src/com/example/projetandroidlp/StrategieCoach.java
@@ -5,12 +5,8 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.content.SharedPreferences;
-import android.content.Intent;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.text.method.LinkMovementMethod;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -21,13 +17,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -39,15 +30,10 @@ import java.util.ArrayList;
 public class StrategieCoach extends Activity implements View.OnClickListener
 {
 
-  /** Log tag.  */
-  private static final String TAG = "BJTrainer/StrategieTrainer";
-
   /** ID pour la boîte de dialogue d'aide.  */
   private static final int DIALOG_AIDE = 0;
   /** ID pour la boîte de dialogue à propos.  */
   private static final int DIALOG_A_PROPOS = 1;
-  /** ID pour la boîte de dialogue reset.  */
-  private static final int DIALOG_RESET = 2;
 
   /** Affichage pour la main du joueur.  */
   private AffichageDeLaMain afficheJoueur;
@@ -70,30 +56,16 @@ public class StrategieCoach extends Activity implements View.OnClickListener
   /** boutton Separer.  */
   private Button btnSeparer;
 
-  /** LaMainler pour les preferences.  */
-  private SharedPreferences pref;
-
   /** Pioche utilisée.  */
   private Pioche deck;
-
-  /** Systematic trainer instance used.  */
-  /*private SystematicTrainer trainer;*/
 
   /** Pile de jeux séparé .  */
   private ArrayList<Jeux> pileJeux;
   /** Jeu courant.  */
   private Jeux jeuCourant;
 
-  /** stratégie optimale.  */
-  private Strategie optimale;
-  /** Si la stratégie calculée est un soft17.  */
-  private boolean h17Strategie;
-
   /** Gains total  */
   private float total;
-
-  /** Regarde si l'utilisateur ne réponds pas par la bonne stratégie.  */
-  private boolean mauvaiseReponse;
 
   /**
    * Creer l'activity.
@@ -103,8 +75,6 @@ public class StrategieCoach extends Activity implements View.OnClickListener
   public void onCreate (Bundle etat)
   {
     super.onCreate (etat);
-    /*PreferenceManager.setDefaultValues (this, R.xml.preferences, false);
-    pref = PreferenceManager.getDefaultSharedPreferences (this);*/
     setContentView (R.layout.main);
 
     CarteImages img = new CarteImages (getResources ());
@@ -131,8 +101,6 @@ public class StrategieCoach extends Activity implements View.OnClickListener
     btnDOUBLER.setOnClickListener (this);
     btnSeparer.setOnClickListener (this);
 
-    //optimale = null;
-    /*trainer = null;*/
     total = 0.0f;
     pileJeux = new ArrayList<Jeux> ();
     lanceNouveauJeu();
@@ -149,7 +117,6 @@ public class StrategieCoach extends Activity implements View.OnClickListener
     etat.putFloat ("total", total);
 
     etat.putInt ("numJeuxs", pileJeux.size ());
-    /*etat.putBoolean ("hasTrainer", trainer != null);*/
     try
       {
         ByteArrayOutputStream byteOut = new ByteArrayOutputStream ();
@@ -159,19 +126,13 @@ public class StrategieCoach extends Activity implements View.OnClickListener
         for (Jeux j : pileJeux)
           out.writeObject (j);
 
-        /*if (trainer != null)
-          out.writeObject (trainer);
-         */
         out.close ();
         byteOut.close ();
-
         etat.putByteArray ("pileJeux", byteOut.toByteArray ());
-        Log.i (TAG, "Sauvegarde de l'état avec succès.");
       }
     catch (IOException exc)
       {
         exc.printStackTrace ();
-        Log.w (TAG, "La sauvegarde de l'état à echoué avec IOException.");
       }
   }
 
@@ -187,10 +148,6 @@ public class StrategieCoach extends Activity implements View.OnClickListener
 
     final int numJeux = etat.getInt ("numJeux");
     final byte[] data = etat.getByteArray ("pileJeux");
-
-    /* Force la ré-intialisation de la strategie.  */
-    optimale = null;
-    /*trainer = null;*/
 
     boolean pasRestaure;
     if (data == null)
@@ -213,28 +170,21 @@ public class StrategieCoach extends Activity implements View.OnClickListener
               j.setPiocheCarte(deck);
               pileJeux.add (j);
             }
-
-          /*if (etat.getBoolean ("hasTrainer"))
-            trainer = (SystematicTrainer) in.readObject ();
-			*/
           
           in.close ();
           byteIn.close ();
 
-          Log.i(TAG, "Etat chargé avec succès.");
         }
       catch (Exception exc)
         {
           pasRestaure = true;
           exc.printStackTrace ();
-          Log.w (TAG, "Chargement de l'état à échoué avec Exception.");
         }
 
     /* Si la restauration à echoué, initialisation à vide.  */
     if (pasRestaure)
       {
         total = 0.0f;
-        /*trainer = null;*/
         pileJeux = new ArrayList<Jeux>();
         lanceNouveauJeu();
       }
@@ -254,34 +204,22 @@ public class StrategieCoach extends Activity implements View.OnClickListener
         return;
       }
 
-    //final Strategie.Decision dec = optimale.decider(jeuCourant);
-
-    assert (jeuCourant.estEnAttente ());
     if (v == btnTirer)
       {
-        /*if (dec != Strategie.Decision.TIRER)
-          alertStrategie (dec);
-        else*/
           jeuCourant.Tirer();
       }
     if (v == btnPasser)
       {
-        /*if (dec != Strategie.Decision.PASSER)
-          alertStrategie(dec);
-        else*/
           jeuCourant.Passer();
       }
     if (v == btnDOUBLER)
       {
         if (!jeuCourant.peutDouble ())
           {
-            Toast t = Toast.makeText (this, getString (R.string.peudouble),
-                                      Toast.LENGTH_SHORT);
+            Toast t = Toast.makeText (this, getString (R.string.peudouble),Toast.LENGTH_SHORT);
             t.setGravity (Gravity.CENTER, 0, 0);
             t.show ();
           }
-        /*else if (dec != Strategie.Decision.DOUBLER)
-          alertStrategie (dec);*/
         else
           jeuCourant.CoupDouble();
       }
@@ -289,17 +227,13 @@ public class StrategieCoach extends Activity implements View.OnClickListener
       {
         if (!jeuCourant.peutSepare ())
           {
-            Toast t = Toast.makeText (this, getString (R.string.peusepare),
-                                      Toast.LENGTH_SHORT);
+            Toast t = Toast.makeText (this, getString (R.string.peusepare),Toast.LENGTH_SHORT);
             t.setGravity (Gravity.CENTER, 0, 0);
             t.show ();
           }
-        /*else if (dec != Strategie.Decision.SEPARER)
-          alertStrategie (dec);*/
         else
           {
-        	Toast t = Toast.makeText (this, getString (R.string.separation),
-                    Toast.LENGTH_SHORT);
+        	Toast t = Toast.makeText (this, getString (R.string.separation),Toast.LENGTH_SHORT);
         	 t.setGravity (Gravity.CENTER, 0, 0);
              t.show ();
             Jeux Separe = jeuCourant.CoupSepare(true);
@@ -327,25 +261,12 @@ public class StrategieCoach extends Activity implements View.OnClickListener
    * @param itm l'item du menu selectionné.
    * @return True si l'évent est géré.
    */
-  @Override
+  @SuppressWarnings("deprecation")
+@Override
   public boolean onOptionsItemSelected (MenuItem itm)
   {
     switch (itm.getItemId ())
       {
-        /*case R.id.show_Strategie:
-          startActivity (new Intent (this, DisplayStrategie.class));
-          return true;
-		*/
-        /*case R.id.preferences:
-          startActivity (new Intent (this, Preferences.class));
-          return true;
-		*/
-        /*case R.id.reset_trainer:
-          deleteFile ("trainer");
-          Log.d (TAG, "Deleted trainer data on local storage.");
-          trainer = null;
-          return true;
-		*/
         case R.id.a_propos:
           showDialog (DIALOG_A_PROPOS);
           return true;
@@ -364,8 +285,8 @@ public class StrategieCoach extends Activity implements View.OnClickListener
 	            @Override
 	            public void onClick(DialogInterface dialog, int which) {
 	            	// Si l'utilisateur a cliqué sur le bouton Oui, on remet à zéro
+	            	total = 0.0f;
 	            	lanceNouveauJeu();
-	            	jeuCourant.resetPayer();
 	            	maj();
 	            }
 	
@@ -433,37 +354,6 @@ public class StrategieCoach extends Activity implements View.OnClickListener
       jeuCourant = pileJeux.remove (pileJeux.size () - 1);
     else
       {
-        /*if (pref.getBoolean ("train", false))
-          {
-            if (trainer == null)
-              {
-                restoreTrainer ();
-                mauvaiseReponse = false;
-              }
-
-            if (mauvaiseReponse)
-              trainer.repeat ();
-            mauvaiseReponse = false;*/
-
-            /* Do this before getNext(), so that the current index is
-               not "lost" if quit before answering it!  */
-            /*saveTrainer ();
-
-            final Jeux next = trainer.getNext (deck, h17);
-            if (next == null)
-              {
-                final String msg = getString (R.string.finished_learning);
-                deleteFile ("trainer");
-                Toast t = Toast.makeText (this, msg, Toast.LENGTH_SHORT);
-                t.setGravity (Gravity.CENTER, 0, 0);
-                t.show ();
-                return;
-              }
-            else
-              jeuCourant = next;
-          }*/
-        /*else
-          {*/
             LaMain joueur = new LaMain ();
             joueur.ajouter (deck.getNouvelleCarte ());
             joueur.ajouter(deck.getNouvelleCarte ());
@@ -472,8 +362,6 @@ public class StrategieCoach extends Activity implements View.OnClickListener
             croupier.ajouter(deck.getNouvelleCarte ());
 
             jeuCourant = new Jeux (joueur, croupier, deck, h17);
-            jeuCourant.resetPayer();
-          //}
       }
 
     assert (jeuCourant != null);
@@ -486,15 +374,6 @@ public class StrategieCoach extends Activity implements View.OnClickListener
   private void majAll ()
   {
     maj();
-    /*if (optimale == null || (jeuCourant.Soft17 != h17Strategie))
-      {
-        optimale = new Strategie ();
-        optimale.remplir (getResources ().getXml (R.xml.strategie_passer17), false);
-        if (jeuCourant.Soft17)
-          optimale.remplir (getResources ().getXml (R.xml.strategie_h17), true);
-
-        h17Strategie = jeuCourant.Soft17;
-      }*/
   }
 
   /**
@@ -545,108 +424,8 @@ public class StrategieCoach extends Activity implements View.OnClickListener
     message.setText (msg);
 
     String extraMsg = "";
-    /*if (!pref.getBoolean ("train", false))
-      extraMsg = String.format (getString (R.string.total_template), total);
-    else if (trainer != null)
-      extraMsg = String.format (getString (R.string.remaining_template),
-                                trainer.getRemainingCount ());*/
     extraMsg = String.format (getString (R.string.total_template), total);
     montant.setText (extraMsg);
   }
-
-  /**
-   *Avertie l'utilisateur sur une autre stratégie optimale.
-   * @param d optimale one.
-   */
-  private void alertStrategie (Strategie.Decision d)
-  {
-    if (jeuCourant.estChange())
-      mauvaiseReponse = true;
-
-    String msg = "";
-    switch (d)
-      {
-        case TIRER:
-          msg = getString (R.string.btnTirer);
-          break;
-        case PASSER:
-          msg = getString (R.string.btnPasser);
-          break;
-        case DOUBLER:
-          msg = getString (R.string.btnDouble);
-          break;
-        case SEPARER:
-          msg = getString (R.string.btnSepare);
-          break;
-        default:
-          assert (false);
-      }
-    //msg = String.format (getString (R.string.suboptimal_decision), msg);
-
-    Toast t = Toast.makeText (this, msg, Toast.LENGTH_SHORT);
-    t.setGravity (Gravity.CENTER, 0, 0);
-    t.show ();
-  }
-
-  /**
-   * Save the Strategie trainer to persistent internal storage.
-   */
-  /*private void saveTrainer ()
-  {
-    try
-      {
-        FileOutputStream fileOut = openFileOutput ("trainer", MODE_PRIVATE);
-        BufferedOutputStream bufferedOut = new BufferedOutputStream (fileOut);
-        ObjectOutputStream out = new ObjectOutputStream (bufferedOut);
-
-        out.writeObject (trainer);
-
-        out.close ();
-        bufferedOut.close ();
-        fileOut.close ();
-
-        Log.d (TAG, "Saved trainer state.");
-      }
-    catch (IOException exc)
-      {
-        exc.printStackTrace ();
-        Log.e (TAG, "Saving trainer to persistent storage failed!");
-      }
-  }*/
-
-  /**
-   * Restore the trainer from local storage, if there is one.  Construct
-   * a new one if there's none.
-   */
-  /*private void restoreTrainer ()
-  {
-    trainer = null;
-    try
-      {
-        FileInputStream fileIn = openFileInput ("trainer");
-        BufferedInputStream bufferedIn = new BufferedInputStream (fileIn);
-        ObjectInputStream in = new ObjectInputStream (bufferedIn);
-
-        trainer = (SystematicTrainer) in.readObject ();
-        Log.d (TAG, "Restored trainer state.");
-
-        in.close ();
-        bufferedIn.close ();
-        fileIn.close ();
-      }
-    catch (FileNotFoundException exc)
-      {
-        /* Do nothing, this is not really a serious error and just means
-           that no trainer was saved yet.  */
-    /*  }
-    catch (Exception exc)
-      {
-        exc.printStackTrace ();
-        Log.e (TAG, "Reading trainer from persistent storage failed!");
-      }
-
-    if (trainer == null)
-      trainer = new SystematicTrainer ();
-  }*/
 
 }
